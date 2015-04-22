1ec93a23a75a68d2017006f9fdbcf182b786f43f
acoross <acoross@gmail.com>
2015-04-22 23:47:00 +0900

    SFX 만 남기고 제거

--- FPRPG/Assets/Scripts/Skill_cli/EnemyEventHandler.cs
+++ FPRPG/Assets/Scripts/Skill_cli/EnemyEventHandler.cs
@@ -26,11 +26,6 @@ public class EnemyEventHandler : MonoBehaviour {
 		GameManager.onEnemyAttacked -= OnEnemyAttackedHandler;
 	}
 
-	void OnEnemyHitEnd()
-	{
-		//enemyAnimator.SetBool("bHit", false);
-	}
-
 	void OnEnemyAttackedHandler()
 	{
 		Debug.Log("Enemy Attacked");
@@ -38,23 +33,14 @@ public class EnemyEventHandler : MonoBehaviour {
 		enemyAnimator.SetBool("bHit", true);
 		m_nHitTime = Time.time + 0.4f;
 
-		// 총알 오브젝트를 만든다
+		// SFX 오브젝트를 만든다
 		GameObject newSFX = (GameObject)Instantiate(lightningPrefab);
-		
-		// 총알의 시작 위치를 설정한다
+		// SFX 출력 위치를 설정한다
 		newSFX.transform.position = sfxSpawnTransform.position;
 		
-		// 새 오브젝트의 PlayerBulletController 컴포넌트를 할당해서 몇 가지 데이터를 지정할 수 있다
-		//PlayerBulletController bullCon = newBullet.GetComponent<PlayerBulletController>();
-		SFXController lghtCon = newSFX.GetComponent<SFXController>();
-
-		// 플레이어 오브젝트를 지정한다
-		//bullCon.playerObject = gameObject;
-		lghtCon.playerObject = gameObject;
-
-		// 총알 발사!
-		//bullCon.launchBullet();    
-		lghtCon.LaunchSFX();
+		SFXController SFXCon = newSFX.GetComponent<SFXController>();
+		SFXCon.playerObject = gameObject;
+		SFXCon.LaunchSFX();
 	}
 
 	// Update is called once per frame
---
+++ FPRPG/Assets/Scripts/Skill_cli/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour 
+{
+	public delegate void EnemyEventHandler();
+	public static event EnemyEventHandler onEnemyAttacked;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("GameManager Start");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		#region SFX text with key input.
+		/*bool bJump = Input.GetButtonDown("Jump");
+
+		if (bJump == true)
+		{
+			Debug.Log ("Jump Clicked");
+			if (onEnemyAttacked != null)
+			{
+				Debug.Log ("onEnemyAttacked call");
+				onEnemyAttacked();
+			}
+		}*/
+		#endregion
+	}		
+
+	void OnGUI()
+	{
+		#region SFX test with button input.
+		if (ShowButton (new Rect(0, 0, 100, 100), "SFX"))
+		{
+			//infoText.text = "Button Clicked";
+
+			if (onEnemyAttacked != null)
+			{
+				Debug.Log ("onEnemyAttacked call");
+				onEnemyAttacked();
+			}
+		}
+		#endregion
+	}
+
+	public bool ShowButton(Rect rect, string text)
+	{
+		GUI.SetNextControlName(text);
+		return GUI.Button (rect, text);
+	}
+}
